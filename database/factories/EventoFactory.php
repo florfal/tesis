914abaa0a2d988b931bfa74c8bcd459e190988aa
Florencia Falcone <florencia.iara.falcone@gmail.com>
2025-01-04 00:45:04 -0300

    Empeze a relizar los seeders de eventos

--- database/factories/EventoFactory.php
+++ database/factories/EventoFactory.php
@@ -17,7 +17,17 @@ class EventoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id'=>id('evento_id'),
+            'titulo'=>string('titulo'),
+            'descricion'=>longText('descripcion'),
+            'ubicacion'=>string('ubicacion'),
+            'dia'=>decimal('dia'),
+            'mes'=>string('mes'),
+            'dia_evento'=>string('dia_evento'),
+            'horario'=>string('horario'),
+            'precio'=>decimal('precio'),
+            'image'=>string('image')->nullable(),
+            'extra'=>string('extra')
         ];
     }
 }
--- database/seeders/EventoSeeder.php
+++ database/seeders/EventoSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,60 @@ class EventoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('eventos')->insert([
+            // [
+            //    'evento_id' =>
+            //    'titulo' =>
+            //    'descripcion'=>
+            //    'ubicacion' =>
+            //    'dia' =>
+            //    'mes' =>
+            //    'dia_evento'=>
+            //    'horario' =>
+            //    'precio' =>
+            //    'imagen' =>
+            //    'extra' =>
+               
+            // ]
+            [
+                'evento_id' => 1,
+                'titulo' => 'Cerámica y Vino',
+                'descripcion'=> 'Únete a nosotros para una experiencia única en la que podrás explorar tu creatividad mientras disfrutas de una copa de vino. Este curso de cerámica es perfecto para relajarte, aprender técnicas básicas y crear tus propias piezas de arte en un ambiente amigable y acogedor.',
+                'ubicacion' => 'Taller Obra, Palermo.',
+                'dia' => 7,
+                'mes' => 'Dic', //esto podria ser lo que ya estan los meses, lo mismo con el dia
+                'dia_evento' => 'Sabados y Domingos',
+                'horario' => '6:00 PM - 9:00 PM',
+                'precio' => 30000,
+                'imagen' => 'img/destacados/ceramica.jpg',
+                'extra' => 'materiales y una copa de vino'
+            ],
+            [
+                'evento_id' => 2,
+                'titulo' => 'Joyería y Piedras',
+            //    'descripcion'=>
+                'ubicacion' => 'El Patio, Berazategui.',
+                'dia' => 10,
+                'mes' => 'Dic',
+                'dia_evento'=> 'Lunes a Viernes',
+                'horario' => '9:00 AM - 12:00 PM',
+                'precio' => 35000,
+                'imagen' => 'img/prox_eventos/joyeria.jpg',
+                'extra' => 'materiales basicos para crear una joya'
+            ],
+            [
+                'evento_id' => 3,
+                'titulo' => 'Show en vivo',
+            //    'descripcion'=>
+            //    'ubicacion' =>
+            //    'dia' =>
+            //    'mes' =>
+            //    'dia_evento'=>
+            //    'horario' =>
+            //    'precio' =>
+            //    'imagen' =>
+            //    'extra' =>
+            ]
+        ]);
     }
 }
