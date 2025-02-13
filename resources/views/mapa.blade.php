c6389acc712dc9437925718352b3c149f8ae3d26
Juli Casaló <julicasalo@MacBook-Pro-de-Juli.local>
2025-02-13 16:25:52 -0300

    Soy el mapa

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -4,10 +4,55 @@
 
 @section('content')
 
-<div>
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26302.959866800447!2d-58.522718499999996!3d-34.5061803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1732678109929!5m2!1ses!2sar" width="375" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <x-nav></x-nav>
+<div style="display: flex; flex-direction: column; height: 100vh; margin: 0;">
+    <!-- Mapa ocupa todo el alto disponible, sin espacio extra -->
+    <div id="map" style="flex-grow: 1; width: 100%;"></div>
+    
+    <!-- Nav ahora está fuera del div del mapa -->
+    <x-nav></x-nav>  
 </div>
+
+<script>
+    (g => {
+        var h, a, k, p = "The Google Maps JavaScript API",
+            c = "google",
+            l = "importLibrary",
+            q = "__ib__",
+            m = document,
+            b = window;
+        b = b[c] || (b[c] = {});
+        var d = b.maps || (b.maps = {}),
+            r = new Set,
+            e = new URLSearchParams,
+            u = () => h || (h = new Promise(async (f, n) => {
+                await (a = m.createElement("script"));
+                e.set("libraries", [...r] + "");
+                for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                e.set("callback", c + ".maps." + q);
+                a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                d[q] = f;
+                a.onerror = () => h = n(Error(p + " could not load."));
+                a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                m.head.append(a);
+            }));
+        d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+    })({
+        key: "{{ env('AIzaSyDdpAhy1chmRwg6sBcmzBHA1O7RxUDgafY') }}",
+        v: "weekly",
+    });
+
+    let map;
+
+    async function initMap() {
+        const { Map } = await google.maps.importLibrary("maps");
+
+        map = new Map(document.getElementById("map"), {
+            center: { lat: -34.5061803, lng: -58.5227185 }, // Ajusta según tu ubicación
+            zoom: 12,
+        });
+    }
+
+    initMap();
+</script>
 
 @endsection
